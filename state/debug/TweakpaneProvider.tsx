b275e5303df8932706379e82654d5996b627198f
David Velasco <davvelsan@gmail.com>
2024-04-04 11:32:39 +0200

    chore: use correct file extension

--- state/debug/TweakpaneProvider.tsx
+++ state/debug/TweakpaneProvider.tsx
@@ -13,6 +13,18 @@ export const PaneContext = createContext<Pane | undefined>(undefined);
 export function TweakpaneProvider({ children }: PropsWithChildren) {
   const [pane, setPane] = useState<Pane>();
 
+  /**
+   * Unfortunately, to initialize the tweakpane instance we need to wait for
+   * Next.js to render the initial html. This means the Pane has to be created
+   * after the first mount.
+   *
+   * Ideally, we'd like to have access to the Pane instance outside of React.
+   * This would enable the atomWith* functions to create the bindings, and so
+   * the useAtomWith* hooks would no longer be necessary. However, this "better"
+   * solution would also have some drawbacks:
+   * - The pane and bindings could not be hidden depending on the pathname.
+   * - No subscriber/listener cleanup on component unmount.
+   */
   useEffect(() => {
     const _pane = new Pane({ title: 'Debug' });
     _pane.registerPlugin(StateBundle);
